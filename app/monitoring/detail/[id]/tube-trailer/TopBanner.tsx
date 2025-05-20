ae264e6b23f3a0c2cbde0996e21b239979cb88df
ddocang <ddocang@gmail.com>
2025-05-20 11:28:22 +0900

    1232

--- app/monitoring/detail/[id]/tube-trailer/TopBanner.tsx
+++ app/monitoring/detail/[id]/tube-trailer/TopBanner.tsx
@@ -79,25 +79,7 @@ const TopBanner: React.FC<TopBannerProps> = ({
           </LogoImageWrapper>
           <span>HyGE&nbsp;Safety&nbsp;Monitoring</span>
         </Logo>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <BannerTitle>튜브 트레일러 모니터링</BannerTitle>
-          {isMobile && (
-            <MobileShortcutButton
-              onClick={() =>
-                router.push(`/monitoring/detail/${params.id}/tube-trailer`)
-              }
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z" />
-              </svg>
-              바로가기
-            </MobileShortcutButton>
-          )}
-        </div>
+        <BannerTitle>튜브 트레일러 모니터링</BannerTitle>
         {!isMobile && (
           <MainMenu>
             <ThemeToggleButton />
